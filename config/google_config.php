852b19174c14dd1164f17d4d5b20e65960f87a94
phuc <nguyendaihoangphuc24@gmail.com>
2024-12-29 22:03:48 +0700

    Tam duoc

--- config/google_config.php
+++ config/google_config.php
@@ -1,7 +1,7 @@
 <?php
 // Google OAuth Configuration
-define('GOOGLE_CLIENT_ID', '702593637962-rijvvltjcsn6bn6ik7424nmr0r0c4otl.apps.googleusercontent.com'); // TODO: Add your Google Client ID
-define('GOOGLE_CLIENT_SECRET', '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'); // TODO: Add your Google Client Secret
+define('GOOGLE_CLIENT_ID', getenv('GOOGLE_CLIENT_ID')); // TODO: Add your Google Client ID
+define('GOOGLE_CLIENT_SECRET', getenv('GOOGLE_CLIENT_SECRET')); // TODO: Add your Google Client Secret
 define('GOOGLE_REDIRECT_URI', 'http://localhost:81/test_windsuft/google-callback.php');
 
 // Required Google OAuth scopes
